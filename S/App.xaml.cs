a4fb1fa7b81b050a30c58106b3bee3ce41e3743a
RaghavSardana1310 <45031684+RaghavSardana1310@users.noreply.github.com>
2018-11-14 13:12:10 +0400

    Add files via upload

---
+++ S/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using S.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace S
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+           AppCenter.Start("5347bb65-205e-4b54-8c80-0e7e2691e403", typeof(Push));
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
